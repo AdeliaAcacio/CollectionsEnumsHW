22457740d30fe205c50070acbf9607fe8ebf5031
Adelia Acacio <2022203@student.cct.ie>
2023-10-18 20:07:16 +0100

    Add Exception, Scanner, While loop
    Added exception to catch error if the file will not be found. Scanner added to print file content.  While Loop was added to check if there is something inside the file.

--- src/collectionsenumshw/CollectionsEnumsHW.java
+++ src/collectionsenumshw/CollectionsEnumsHW.java
@@ -4,22 +4,45 @@
  */
 package collectionsenumshw;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
- *Mission: Create a program that performs 
- * @author AdeliaAcacio
+ *Mission: Create a program that performs read a file, store, 
+ * create teams and print the name of the teams without duplicate members in each teams
+ * 
+ * @author AdeliaAcacio2022203
  */
 public class CollectionsEnumsHW {
 
     /**
      * @param args the command line arguments
      * 
-     * GitHub Link 
+     * GitHub Link: https://github.com/AdeliaAcacio/CollectionsEnumsHW
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("The File can note be founded!");
+        
+        String mockDataFile = "MOCK_DATA.csv";
+        int numOfMembers;
         
         
+        Scanner mFile = new Scanner(new FileReader(mockDataFile));
         
-    }
+        //While Loop will check if there is something inside the file
+        while (mFile.hasNext()){
+            
+            String input = mFile.nextLine(); //Reads a line from the mFile
+            System.out.println("The data in the file: " + input);//Print the content insed the file
+             
+            
+        }
+        System.out.println("Finished to read the file!");//After read all the file this message will be printed
+        }
+    
+    
     
 }
